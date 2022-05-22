feb6ebcbbb0a730de9f38ad85a2e259cd4e7a795
younsang <younsang.jo@gmail.com>
2022-05-22 14:34:13 +0900

    feat: 0410_events add

---
+++ 0410_events/src/components/Test.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script setup>
+  // import { reactive } from 'vue'
+
+  // const students = reactive {
+  //   no: 3,
+  //   name: '윤상',
+  //   age: 29
+  // }
+</script>
